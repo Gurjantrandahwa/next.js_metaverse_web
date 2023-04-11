1dcd080a0246d952dc2c53e714debd826185cf47
Gurjant Singh <gurjantrandhawa74456@gmail.com>
2023-04-11 20:58:53 +0530

    Next.js Website

--- metaverse_web/app/page.js
+++ metaverse_web/app/page.js
@@ -5,13 +5,29 @@ const Page = () => (
   <div className='bg-primary-black overflow-hidden'>
     <Navbar />
     <Hero />
-    <About />
+    <div className='relative'>
+      <About />
+      <div className='gradient-03 z-0'>
     <Explore />
-    <GetStarted />
-    <WhatsNew />
+      </div>
+    </div>
+    <div className='relative'>
+      <GetStarted />
+      <div className='gradient-04 z-0'>
+  <WhatsNew />
+      </div>
+  
+ </div>
+  
     <World />
-    <Insights />
-    <Feedback />
+    <div className='gradient-03 z-0'>
+      <Insights />
+      <div className='gradient-04 z-0'>
+          <Feedback />
+            </div>
+  
+       </div>
+  
     <Footer />
   </div>
 );
--- metaverse_web/sections/About.jsx
+++ metaverse_web/sections/About.jsx
@@ -1,7 +1,7 @@
 'use client';
 import {motion} from "framer-motion";
 import styles from "../styles";
-import { staggerContainer } from "../utils/motion";
+import { fadeIn, staggerContainer } from "../utils/motion";
 import { TypingText } from "../components";
 const About = () => (
   <section
@@ -16,11 +16,33 @@ const About = () => (
         className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
       >
         <TypingText title="| About Metaversus"
-
         textStyles="text-center"
-/>
+      />
+      
+      <motion.p
+        variants={fadeIn('up', 'tween', 0.2, 1)}
+        className="mt-[8px] font-normal sm:text-[32px] text-[20px] text-center text-secondary-white"
+      >
+        <span className="font-extrabold tex-white">Metaverse{" "}</span>
+        is a new thing in the future, where you can enjoy the virtual world by feeling like it's really real, you can feel what you feel in this metaverse world, because this is really the
+       <span className="font-extrabold tex-white"> madness of the metaverse </span> 
+        of today, using only
+        <span className="font-extrabold tex-white"> VR </span>
+        devices you can easily
+        <span className="font-extrabold tex-white"> explore </span>
+        the metaverse world you want, turn your dreams into reality. Let's explore the madness of the metaverse by scrolling down
+      </motion.p>
+
+          <motion.img
+      variants={fadeIn('up', 'tween', 0.3, 1)}
+      src="/arrow-down.svg"
+      alt="arrow-down"
+      className="w-[18px] h-[24px] object-contain mt-[28px]"
+    />
       </motion.div>
 
+
+      
   </section>
 );
 
